1f0864b538c02f0df956f35858e5ae467dd4d60d
Kalusa93 <lucaslegui21@gmail.com>
2024-03-29 04:14:52 -0300

    added jupiter notebook

--- movies_project.py
+++ movies_project.py
@@ -16,7 +16,7 @@ pd.options.mode.chained_assignment = None
 
 # Now we need to read in the data
 df = pd.read_csv('movies.csv')
-# print(df)
+# 1) print(df)
 
 # Missing data
 # We need to see if we have any missing data
@@ -25,3 +25,40 @@ df = pd.read_csv('movies.csv')
 for col in df.columns:
     pct_missing = np.mean(df[col].isnull())
     print('{} - {}%'.format(col, round(pct_missing*100)))
+
+# 2) print(df.dtypes)
+# df['budget'] = df['budget'].astype('int64')
+    
+df['budget'] = pd.to_numeric(df['budget'], errors='coerce').fillna(0).astype(int)
+df['gross'] = pd.to_numeric(df['gross'], errors='coerce').fillna(0).astype(int) 
+
+df_ordered_by_gross = df.sort_values(by=['gross'], inplace=False, ascending=False)
+# 3) print(df_ordered_by_gross)
+
+# Drop any duplicates
+df.drop_duplicates()
+
+# Scatter plot with budget vs gross
+""" 4) plt.scatter(x=df['budget'], y=df['gross'])
+    plt.show() """
+
+# We see that a few gross values are negative so we'll drop them
+df = df[df['gross'] >= 0]
+
+plt.scatter(x=df['budget'], y=df['gross'])
+plt.title('Budget vs Gross earnings')
+plt.xlabel('Budget')
+plt.ylabel('Gross earning')
+# plt.show()
+
+# Plot budget vs gross using seaborn
+sns.regplot(x="budget", y="gross", data=df, scatter_kws={"color":"red"}, line_kws={"color":"blue"})
+plt.show()
+
+# Let's look at correlation
+df.corr()
+
+# There is a high correlation between budget and gross
+
+correlation_matrix = df.corr()
+sns.heatmap(correlation_matrix, annot=True)
